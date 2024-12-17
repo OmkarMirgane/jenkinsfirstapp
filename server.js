94138f30e64144397a2b8109457c3dbf174cafc9
sunbeam <omkarmirgane2305@gmail.com>
2024-12-17 20:31:15 +0530

    update in port

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-// Import required modules
+// // Import required modules
+// const express = require('express');
+// const path = require('path');
+
+// // Create an Express application
+// const app = express();
+
+// // Define a route to serve the HTML file
+// app.get('/', (req, res) => {
+//     // Send the HTML file as the response
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// // Start the server
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
 const express = require('express');
-const path = require('path');
-
-// Create an Express application
 const app = express();
+const port = 4000;  // Should match the port exposed in the Dockerfile
 
-// Define a route to serve the HTML file
 app.get('/', (req, res) => {
-    // Send the HTML file as the response
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('Hello World!');
 });
 
-// Start the server
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
